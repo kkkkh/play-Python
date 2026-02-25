7f22bb87b2abf98cb75be8df9043b64e44320b4f
kkkkh <n290022398@gmail.com>
2026-02-25 18:12:39 +0800

    llm

--- LLM/torch/index.py
+++ LLM/torch/index.py
@@ -1,9 +1,9 @@
 import torch
 print(torch.__version__)
 # torch.__version__
-torch.cuda.is_available()
-print(torch.cuda.is_available())
+# torch.cuda.is_available()
+# print(torch.cuda.is_available())
 
 
-tensor1d = torch.tensor([1, 2, 3])
-print(tensor1d.dtype)
+# tensor1d = torch.tensor([1, 2, 3])
+# print(tensor1d.dtype)
